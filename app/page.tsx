b0d805462be26627bca96a8c64ea5b8d1af28956
GameTec_live <benedikt.werner08@gmail.com>
2024-09-06 23:11:00 +0200

    feat: working map

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,14 @@ import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "@/components/ui/resizable"
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+} from "@/components/ui/resizable";
+import dynamic from 'next/dynamic'
+import { Suspense } from "react";
+
+const DynamicMap = dynamic(() => import('../components/map'), {
+  ssr: false,
+  loading: () => <p>A map is loading</p>,
+})
 
 export default function Home() {
   /*return (
@@ -23,23 +29,18 @@ export default function Home() {
   return (
     <ResizablePanelGroup direction="horizontal" className="w-full h-full">
       <ResizablePanel defaultSize={25}>
-        <div className="flex h-screen items-center justify-center p-6">
-          <span className="font-semibold">Sidebar</span>
+        <div className="flex h-screen">
+          <header>
+            <TopBar />
+          </header>
+          <main>
+            
+          </main>
         </div>
       </ResizablePanel>
       <ResizableHandle />
-      <ResizablePanel className="flex h-screen items-center justify-center p-6">
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
+      <ResizablePanel defaultSize={75}>
+        <DynamicMap />
       </ResizablePanel>
     </ResizablePanelGroup>
   );
